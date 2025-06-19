2b85b4f9bf37213c3129f95e4e0b9be4191a8bc7
Wb2d1234 <danilselutin1@gmail.com>
2025-06-19 14:09:03 +0300

    v1

--- cmd/main.go
+++ cmd/main.go
@@ -32,11 +32,9 @@ func main() {
 	userRepo := models.NewUserRepository(pg.DB)
 	log.Printf("Loaded config: DB_DSN=%s, JWT_SECRET=%s, PORT=%s", cfg.DB_DSN, cfg.JWT_SECRET, cfg.PORT)
 
-	// Маршруты
 	http.Handle("/register", handlers.RegisterHandler(userRepo))
 	http.Handle("/login", handlers.LoginHandler(userRepo, cfg.JWT_SECRET))
 
-	// Защищённый маршрут
 	http.Handle("/profile", middleware.AuthMiddleware(cfg.JWT_SECRET)(handlers.ProfileHandler()))
 
 	log.Printf("Server starting on port %s", cfg.PORT)
